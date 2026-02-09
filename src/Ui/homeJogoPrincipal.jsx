60398aa0dc42d13b20c38d629f2c99cf93e9c1aa
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-09 15:27:58 -0300

    novo componete e css

---
+++ src/Ui/homeJogoPrincipal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../src/Css/homeJogoPrincipal.css"
+
+type Props = {}
+
+const homeJogoPrincipal = (props: Props) => {
+  return (
+    <div>homeJogoPrincipal</div>
+  )
+}
+
+export default homeJogoPrincipal
