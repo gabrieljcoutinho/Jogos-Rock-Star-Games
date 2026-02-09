77dbdde9714b3f0817247ee5aa12ca463bf3c30e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-09 16:24:22 -0300

    cotneudo novo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomeJogoPrincipal from "../Ui/homeJogoPrincipal.jsx"
 import JogoEmDestaque from "../Ui/JogoEmDestaque.jsx"
+import UltimasNoticias from "../Ui/UltimasNoticias.jsx"
 
 const Home = () => {
   return (
@@ -9,6 +10,8 @@ const Home = () => {
       <HomeJogoPrincipal />
 
       <JogoEmDestaque />
+
+      <UltimasNoticias />
     </div>
   )
 }
