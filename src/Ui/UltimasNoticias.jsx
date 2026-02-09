4b577932ba90636e2782b3a6572a54e8a99afaae
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-09 16:29:12 -0300

    classse

--- src/Ui/UltimasNoticias.jsx
+++ src/Ui/UltimasNoticias.jsx
@@ -7,6 +7,10 @@ const UltimasNoticias = (props: Props) => {
   return (
     <div className='ultimasNoticiasConteudo'>
         <h3 className='ultimaNoticia'>Ultimas Noticias</h3>
+
+        <div className="conteudo">
+            
+        </div>
     </div>
   )
 }
