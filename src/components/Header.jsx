88a3f71188d0968af6805e40280bd0b59743cec4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-09 15:07:24 -0300

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../Css/Header.css"
 
+import logoHeader from "../../src/img/Logo.png"
+
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <div className='header'>
+      <img src={logoHeader} alt="" />
       <ul>
       <li>Jogos</li>
       <li>Loja</li>
