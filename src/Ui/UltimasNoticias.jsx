d010aac9d1def4d6f6f63e7517a3a0ab8af6adfb
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 13:11:33 -0300

    imgs

--- src/Ui/UltimasNoticias.jsx
+++ src/Ui/UltimasNoticias.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import "../../src/Css/ultimasNoticias.css"
+import React from 'react';
+import "../../src/Css/ultimasNoticias.css";
 
-import gta from "../../src/img/GTA 6.png"
+// Importe as outras imagens aqui também
+import gta from "../../src/img/GTA 6.png";
+import img2 from "../../src/img/trailer.png"
+import img3 from "../../src/img/gtaOnline.png"
 
 type Props = {}
 
 const UltimasNoticias = (props: Props) => {
   return (
     <div className='ultimasNoticiasConteudo'>
-        <h3 className='ultimaNoticia'>Ultimas Noticias</h3>
+      <h3 className='ultimaNoticia'>Ultimas Noticias</h3>
 
-        <div className="conteudo">
-                    <div className="gta">
-                        <img src={gta} alt="" className='imggta' />
-                    </div>
+      <div className="conteudo">
+        {/* Imagem Principal (Esquerda) */}
+        <div className="card gta-principal">
+          <img src={gta} alt="GTA 6" className='img-grid' />
         </div>
+
+        {/* Imagem Superior (Direita) */}
+        <div className="card noticia-menor">
+          <img src={img2} alt="Noticia 2" className='img-grid' />
+        </div>
+
+        {/* Imagem Inferior (Direita) */}
+        <div className="card noticia-menor">
+          <img src={img3} alt="Noticia 3" className='img-grid' />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UltimasNoticias
+export default UltimasNoticias;
